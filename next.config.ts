1c50659891e94330fa54205ef57b284ab355b2d2
Abhishek Bandhu <abhishekbandhu@gmail.com>
2026-03-10 17:38:13 +0530

    too many redirects issue

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.svgcrawler.com',
+          },
+        ],
+        destination: 'https://svgcrawler.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
