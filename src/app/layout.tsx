61362f948161120e7a345e1c6c17ca8e89d086ff
Abhishek Bandhu <abhishekbandhu@gmail.com>
2026-06-01 10:08:17 +0530

    Add Google AdSense script globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,6 +84,7 @@ export default function RootLayout({
             `,
           }}
         />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6233070402973325" crossOrigin="anonymous"></script>
       </body>
     </html>
   );
